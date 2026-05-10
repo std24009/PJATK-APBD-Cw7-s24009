082dd617f0b8982ba290918a4264a4a02e247f84
HockIt <mduch028@gmail.com>
2026-05-10 16:16:21 +0200

    Added collections in DatabaseContext

--- EFCoreCodeFirst/EFCoreCodeFirst/Infrastructure/DatabaseContext.cs
+++ EFCoreCodeFirst/EFCoreCodeFirst/Infrastructure/DatabaseContext.cs
@@ -1,8 +1,13 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using EFCoreCodeFirst.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace EFCoreCodeFirst.Infrastructure;
 
 public class DatabaseContext(DbContextOptions opt) : DbContext(opt)
 {
-    
+    public DbSet<ComponentTypes> ComponentTypes { get; set; }
+    public DbSet<ComponentManufacturers> ComponentManufacturers { get; set; }
+    public DbSet<Components> Components { get; set; }
+    public DbSet<PCs> PCs { get; set; }
+    public DbSet<PCComponents> PcComponents { get; set; }
 }
