1a1c51f693abbc2aca2a4966aaa22ad1116e8920
HockIt <mduch028@gmail.com>
2026-05-17 12:56:47 +0200

    Added scoped Services

--- EFCoreCodeFirst/EFCoreCodeFirst/Program.cs
+++ EFCoreCodeFirst/EFCoreCodeFirst/Program.cs
@@ -1,4 +1,5 @@
 using EFCoreCodeFirst.Infrastructure;
+using EFCoreCodeFirst.Service;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -8,6 +9,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+builder.Services.AddScoped<IPCsService, PCsService>();
 
 builder.Services.AddDbContext<DatabaseContext>(opt =>
 {
